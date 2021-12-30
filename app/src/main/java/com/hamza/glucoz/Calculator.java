2a2429ca09e4e360808d5ca0e196e0201269e1e3
EL-BOURISSI HAMZA <hamza.elbourissi.10@gmail.com>
2021-12-30 23:32:21 +0100

    addGlycemi

--- app/src/main/java/com/hamza/glucoz/Calculator.java
+++ app/src/main/java/com/hamza/glucoz/Calculator.java
@@ -6,13 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Calculator extends AppCompatActivity {
     EditText put;
     TextView out;
+    ImageView cnv;
 
 
     @SuppressLint("SetTextI18n")
@@ -24,15 +30,39 @@ public class Calculator extends AppCompatActivity {
 
         put=findViewById(R.id.id_calculPut);
         out=findViewById(R.id.id_calculOut);
+        cnv=findViewById(R.id.id_btnConvert);
 
-        //String Put=put.getText().toString().trim();
-        double p= Double.valueOf(put.getText().toString());
-        double o ;
-        o = (p* 7) / 1.5;
+        cnv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Put = put.getText().toString().trim();
+                if(TextUtils.isEmpty(Put)){
+                    Toast.makeText(Calculator.this, "Add value", Toast.LENGTH_SHORT).show();
+                }else{
+                    double p = Float.parseFloat(Put);
+                    out.setText(""+((p * 7)/150));
 
-       out.setText(Double.toString(o));
+                }
 
 
+
+
+
+            }
+        });
+
+        //dd
+        //double Put=put.getText().toString().trim();
+        //double p=Double.parseDouble(Put);
+        //double o = (* 7) / 1.5;
+
+        //out.setText(Double.toString(o));
+
+        //String Put=put.getText().toString();
+        //double p = Double.parseDouble(put.getText().toString());
+        //double o = (p*7)/1.5;
+        //String r = Double.toString(o);
+        //out.setText(r);
     }
 
 
